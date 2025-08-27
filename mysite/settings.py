a4c9a0e16cdb99cf3785ab5ba461fc0bf71f16a7
tawfik-ghamian <tawfik.gh@businesscode-tech.com>
2025-08-27 19:46:25 +0300

    delete rest framework

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,8 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'analysis', 
-    'rest_framework',
+    'analysis',
 ]
 
 MIDDLEWARE = [
