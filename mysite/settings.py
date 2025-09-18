b3b796f12e2df5dcc3dd99ae1b8c5e7165630896
tawfik-ghamian <tawfik.gh@businesscode-tech.com>
2025-09-18 16:00:55 +0300

    edit urlls

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,14 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk'
+SECRET_KEY = os.environ.get("SECRET_KEY",'')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 APPEND_SLASH=False
 
-ALLOWED_HOSTS = ['https://web-production-e39f0.up.railway.app']
+ALLOWED_HOSTS = ['web-production-e39f0.up.railway.app','localhost','127.0.0.1','redis.railway.internal']
 
 # Media
 
@@ -37,7 +37,7 @@ MEDIA_URL = '/media/'
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
-CSRF_TRUSTED_ORIGINS = ["https://web-production-e39f0.up.railway.app/video/upload/"]
+CSRF_TRUSTED_ORIGINS = ["https://web-production-e39f0.up.railway.app",]
 
 # Application definition
 
